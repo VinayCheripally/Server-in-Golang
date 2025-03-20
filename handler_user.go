1e36f299259cf4571634fdac450e57b6b2ecd5cf
VinayCheripally <vinaycheripally1@gmail.com>
2025-03-20 12:05:46 +0530

    added api_key

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/VinayCheripally/Server_in_Golang/internal/auth"
 	"github.com/VinayCheripally/Server_in_Golang/internal/database"
 	"github.com/google/uuid"
 )
@@ -34,5 +35,17 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+	}
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("could not get user: %v", err))
+	}
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
---
+++ internal/auth/auth.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+func GetAPIKey(headers http.Header) (string, error) {
+	val := headers.Get("Authorization")
+	if val == "" {
+		return "", errors.New("no authorization key")
+	}
+
+	vals := strings.Split(val, " ")
+	if len(vals) != 2 {
+		return "", errors.New("malformed auth header")
+	}
+	if vals[0] != "APIKey" {
+		return "", errors.New("malformed auth header")
+	}
+
+	return vals[1], nil
+}
